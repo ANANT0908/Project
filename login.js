e074b0b224936a62ebe111b92a9ec04801f87cbc
ANANT0908 <88321054+ANANT0908@users.noreply.github.com>
2021-08-02 16:42:08 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+
+function login(){
+  var email= document.getElementById('email'); 
+  var password=document.getElementById('password');
+
+  if (email == localStorage.getItem("e") && password == localStorage.getItem("p")){
+    document.location.href = "file:///C:/Users/ANANT%20VERMA/Documents/GitHub/Signup-and-login-page-redirect-to-responsive-CV-.github.io/cv.html";
+    return false;
+  }
+  else{
+    alert("Invalid Email and Password");
+  }
+
+}
+
