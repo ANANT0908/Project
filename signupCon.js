a1dc3fe87429645d1603f43c26165e60200a74a1
ANANT0908 <88321054+ANANT0908@users.noreply.github.com>
2021-08-03 19:43:44 +0530

    Add files via upload

---
+++ signupCon.js
@@ -0,0 +1,6 @@
+
+function login(){
+      document.location.href ='file:///C:/Users/ANANT%20VERMA/Documents/GitHub/Signup-and-login-page-redirect-to-responsive-CV-.github.io/login.html';
+      return false;
+  }
+  
