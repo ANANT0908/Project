6f0c8734b35849e6f0dd657101d9d23e5dfd7375
ANANT0908 <88321054+ANANT0908@users.noreply.github.com>
2021-08-04 20:04:08 +0530

    Update login.js

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ function login(){
   var password=document.getElementById('password');
 
   var getJSON1 = localStorage.getItem('email');
-  var getJSON2 = localStorage.getItem('email');
+  var getJSON2 = localStorage.getItem('password');
 
   if (email == getJSON1 && password == getJSON2){
     document.location.href = "file:///C:/Users/ANANT%20VERMA/Documents/GitHub/Signup-and-login-page-redirect-to-responsive-CV-.github.io/cv.html";
